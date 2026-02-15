080ca8fd4470b69732622c3d728276ab23ba7322
Jonas A. Hultén <jonas@sldr.se>
2026-02-15 19:56:42 +0100

    chore: flake rudiments

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+          ];
+
+          shellHook = ''
+            source .secrets
+          '';
+        };
+      }
+    );
+}
